bc2fc9997694b7ea9cc23d0a477e0fc5488739fb
Vladimir Zorov <vl.zorov@gmail.com>
2023-02-20 19:25:05 +0200

    Initial commit

---
+++ src/Bomb.java
@@ -0,0 +1,22 @@
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Bomb {
+    public static void main(String[] args) {
+
+       Scanner scanner = new Scanner(System.in);
+
+            ArrayDeque<Integer> bombEffects = new ArrayDeque<>();
+            ArrayDeque <Integer> bombCasing = new ArrayDeque<>();
+
+            Arrays.stream(scanner.nextLine().split(", "))
+                    .mapToInt(Integer::parseInt).forEach(bombEffects::offer);
+            Arrays.stream(scanner.nextLine().split(", "))
+                    .mapToInt(Integer::parseInt).forEach(bombCasing::push);
+
+
+            System.out.println();
+
+    }
+}
