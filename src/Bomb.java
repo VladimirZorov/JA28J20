283903b324a2042183cf297398f3bc1458db079b
Vladimir Zorov <vl.zorov@gmail.com>
2023-02-21 19:58:51 +0200

    add

--- src/Bomb.java
+++ src/Bomb.java
@@ -3,15 +3,15 @@ import java.util.*;
 public class Bomb {
     public static void main(String[] args) {
 
-       Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-            ArrayDeque<Integer> bombEffects = new ArrayDeque<>();
-            ArrayDeque <Integer> bombCasing = new ArrayDeque<>();
+        ArrayDeque<Integer> bombEffects = new ArrayDeque<>();
+        ArrayDeque<Integer> bombCasing = new ArrayDeque<>();
 
-            Arrays.stream(scanner.nextLine().split(", "))
-                    .mapToInt(Integer::parseInt).forEach(bombEffects::offer);
-            Arrays.stream(scanner.nextLine().split(", "))
-                    .mapToInt(Integer::parseInt).forEach(bombCasing::push);
+        Arrays.stream(scanner.nextLine().split(", "))
+                .mapToInt(Integer::parseInt).forEach(bombEffects::offer);
+        Arrays.stream(scanner.nextLine().split(", "))
+                .mapToInt(Integer::parseInt).forEach(bombCasing::push);
 
         Map<String, Integer> bombTypes = new HashMap<>();
         bombTypes.put("Datura Bombs", 0);
@@ -32,19 +32,19 @@ public class Bomb {
 
             if (sumComponents == 40) {
                 bombTypes.get("Datura Bombs");
-                bombTypes.compute("Datura Bombs",(s, integer) -> +1);
+                bombTypes.compute("Datura Bombs", (s, integer) -> +1);
                 bombEffects.poll();
                 bombCasing.pop();
                 count1 += 1;
             } else if (sumComponents == 60) {
                 bombTypes.get("Cherry Bombs");
-                bombTypes.compute("Cherry Bombs",(s, integer) -> +1);
+                bombTypes.compute("Cherry Bombs", (s, integer) -> +1);
                 bombEffects.poll();
                 bombCasing.pop();
                 count2 += 1;
             } else if (sumComponents == 120) {
                 bombTypes.get("Smoke Decoy Bombs");
-                bombTypes.compute("Smoke Decoy Bombs",(s, integer) -> +1);
+                bombTypes.compute("Smoke Decoy Bombs", (s, integer) -> +1);
                 bombEffects.poll();
                 bombCasing.pop();
                 count3 += 1;
@@ -54,31 +54,31 @@ public class Bomb {
                 bombCasing.addFirst(redusedBombCasing);
             }
 
-            if (count1 >= 3 && count2 >=3 && count3 >= 3) {
+            if (count1 >= 3 && count2 >= 3 && count3 >= 3) {
                 havThreeOfEach = true;
             }
         }
 
-          if (havThreeOfEach) {
-              System.out.println("Bene! You have successfully filled the bomb pouch!");
-          } else {
-              System.out.println("You don't have enough materials to fill the bomb pouch.");
-          }
+        if (havThreeOfEach) {
+            System.out.println("Bene! You have successfully filled the bomb pouch!");
+        } else {
+            System.out.println("You don't have enough materials to fill the bomb pouch.");
+        }
 
-          if (bombEffects.size() == 0) {
-              System.out.println("Bomb Effects: empty");
-          } else {
-              System.out.print("Bomb Effects: ");
-              for (int i = 0; i < bombEffects.size(); i++) {
-                  System.out.println(bombEffects.pop());
-              }
-          }
+        if (bombEffects.size() == 0) {
+            System.out.println("Bomb Effects: empty");
+        } else {
+            StringBuilder sb = new StringBuilder();
+            System.out.print("Bomb Effects: ");
+            sb.append(bombEffects).append(", ");
+            System.out.println(sb.toString().trim());
+        }
 
-          if (bombCasing.size() ==0) {
-              System.out.println("Bomb Casings: empty");
-          } else {
+        if (bombCasing.size() == 0) {
+            System.out.println("Bomb Casings: empty");
+        } else {
 
-          }
+        }
 
         System.out.printf("Cherry Bombs: %d%n", count2);
         System.out.printf("Datura Bombs: %d%n", count1);
